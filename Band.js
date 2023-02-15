8c955ee41b6cc1df32e1d7e5eed9b3489195145d
harryg15 <harry.grant99@googlemail.com>
2023-02-15 16:45:51 +0000

    added all tests (with bonus tests included)

--- Band.js
+++ Band.js
@@ -3,7 +3,8 @@ const {Sequelize, sequelizeCon} = require('./db');
 // TODO - define the Band model
 let Band = sequelizeCon.define("band", {
     name: Sequelize.STRING,
-    genre: Sequelize.STRING
+    genre: Sequelize.STRING,
+    showCount: Sequelize.INTEGER
 });
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -15,19 +15,55 @@ describe('Band and Musician Models', () => {
     test('can create a Band', async () => {
         // TODO - test creating a band
 
-        const createBand = await Band.create({ name: "Coldplay", genre: "Alternative"})
+        await Band.create({ name: "Coldplay", genre: "Alternative"})
         const foundBand = await Band.findAll()
 
-        expect(foundBand[0].name).toBe("Coldplay");
-        expect(foundBand[0].genre).toBe("Alternative");
+        expect(foundBand.length).toEqual(1)
+        
+    })
+
+    test('add showCount property to Band', async () => {
+        // TODO - test creating a band
+        
+        await Band.create({ name: "Calvin Harris", genre: "Dance", showCount: 1})
+        const foundBand = await Band.findByPk(2)
+        
+        expect(foundBand.dataValues.showCount).toEqual(1)
+
     })
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
-        const createMusician = await Musician.create({ name: "Chirs Martin", instrument: "Piano"})
+
+        await Musician.create({ name: "Chris Martin", instrument: "Piano"})
+
         const foundMusician = await Musician.findAll()
 
-        expect(foundMusician[0].name).toBe("Chirs Martin");
+        expect(foundMusician[0].name).toBe("Chris Martin");
         expect(foundMusician[0].instrument).toBe("Piano");
+    })
+
+    test('can update a Musician', async () => {
+        // TODO - test creating a musician
+
+        await Musician.update({name: "Jimmy Hendrix", instrument: "Guitar"}, 
+        {where: {name: "Chris Martin", instrument: "Piano"}})
+
+        const foundMusician = await Musician.findAll()
+
+        expect(foundMusician[0].name).toBe("Jimmy Hendrix");
+        expect(foundMusician[0].instrument).toBe("Guitar");
+    })
+
+    test('can delete a Musician', async () => {
+        // TODO - test creating a musician
+
+        const newMusician = await Musician.create({ name: "Beyonce", instrument: "voice"})
+        
+        await newMusician.destroy()
+
+        const foundMusician = await Musician.findAll()
+        
+        expect(foundMusician.length).toEqual(1)
     })
 })
